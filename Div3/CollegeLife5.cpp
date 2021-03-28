4534ee7830c778b8b0c096f3d8c11207ce75e61e
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-03-28 18:59:15 +0530

    Update CollegeLife5.cpp

--- Div3/CollegeLife5.cpp
+++ Div3/CollegeLife5.cpp
@@ -1,3 +1,12 @@
+/*
+There is only 1 TV in the common room, and as luck would have it, both the El Clasico football match and the India-Pakistan cricket match are happening simultaneously.
+
+There is one football fan who is looking at the live commentary online and will scream whenever an 'important' event takes place in the El Clasico. Similarly, there is a cricket fan who will do so for every important event in the cricket match.
+
+You are given two arrays - F1,F2,…,Fn, which denote the times when an important event happens in the football match. And similarly C1,C2,…,Cm for cricket. You sadly have the remote in hand. You start out by watching the El Clasico. But whenever an Important event happens in the sport which isn't on the TV right now, you will be forced to switch to that sport's channel, and this continues, i.e., you switch channels if and only if when an important event in the other sport happens. Find the total number of times you will have to switch between the channels.
+*/
+
+
 #include<bits/stdc++.h>
 #include<map>
 

1fb40d1e81044835402c46c6c787a44e748b6228
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-03-28 19:00:30 +0530

    Update PointGrid.cpp

--- Div3/PointGrid.cpp
+++ Div3/PointGrid.cpp
@@ -1,3 +1,10 @@
+/*
+You are the Chief Engineer of a fast growing start-up. You wish to place K servers in some distinct locations from among N locations. The locations, numbered 1,2,…,N are arranged in a circular manner. The distance between every adjacent location is 1 unit. 1 and N are adjacent.
+
+You want to place the servers such that the maximum shortest distance between any two adjacent servers is as less as possible. Find this minimum possible distance that can be achieved, and also the minimum number of pairs of adjacent servers separated by this distance.
+*/
+
+
 #include<bits/stdc++.h>
 
 using namespace std;
